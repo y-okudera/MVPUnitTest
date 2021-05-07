53e39bd10993dbc071a7dd124182dfb391bbd66b
okudera <appledev.yuoku@gmail.com>
2021-05-07 17:17:54 +0900

    Unit testing the GitHubUsersTranslator.

--- DomainTests/Utility/GitHubUserEntity/GitHubUserEntity+.swift
+++ DomainTests/Utility/GitHubUserEntity/GitHubUserEntity+.swift
@@ -1,8 +1,8 @@
 //
 //  GitHubUserEntity+.swift
-//  DataTests
+//  DomainTests
 //
-//  Created by okudera on 2021/05/06.
+//  Created by okudera on 2021/05/07.
 //
 
 import Data
@@ -16,16 +16,6 @@ extension GitHubUserEntity {
             avatarUrl: "https://avatars.githubusercontent.com/u/1?v=4",
             htmlUrl: "https://github.com/mojombo",
             reposUrl: "https://api.github.com/users/mojombo/repos"
-        )
-    }
-
-    static var testOtherObject: GitHubUserEntity {
-        GitHubUserEntity(
-            login: "defunkt",
-            id: 2,
-            avatarUrl: "https://avatars.githubusercontent.com/u/2?v=4",
-            htmlUrl: "https://github.com/defunkt",
-            reposUrl: "https://api.github.com/users/defunkt/repos"
         )
     }
 }
